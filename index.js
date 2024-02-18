d4c0bc22b5ff3682cabf9a0b748abe5b305fa76c
C4rolanne <126annne@gmail.com>
2024-02-18 11:59:24 -0300

    firt commit

---
+++ index.js
@@ -0,0 +1,10 @@
+let resultado = soma(5, 10)
+
+
+function soma(numA, numB){
+    let somatorio = numA + numB 
+    console.log(somatorio)
+    return somatorio 
+}
+
+console.log("O resultado dessa função é " + resultado)
